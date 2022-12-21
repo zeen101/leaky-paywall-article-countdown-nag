4835d7dead102c2a1607a7e45fc68aa954111b05
Jeremy Green <jeremygreen@Jeremys-MacBook-Pro.local>
2022-12-21 09:12:41 -0600

    set cookie for redirect on zero nag

--- js/article-countdown-nag.js
+++ js/article-countdown-nag.js
@@ -69,5 +69,56 @@ $lpacdn_jquery( document ).ready( function($) {
         e.preventDefault();
         $( 'div#issuem-leaky-paywall-articles-remaining-nag' ).animate({ left:'-351px' });
     });
+
+    $(document).on('click', '#issuem-leaky-paywall-articles-zero-remaining-nag a', function(e) {
+        e.preventDefault();
+
+        var url = $(this).attr('href');
+        var post_id = '';
+        var nag_loc = '';
+        var bodyClasses = $('body').attr('class').split(' ');
+
+        $.each(bodyClasses, function(i, value) {
+
+            if ( !value.search('postid' ) ) {
+                
+                var classArray = value.split('-');
+
+                var post_id = parseInt( classArray[1] );
+
+                if ( post_id > 0 ) {
+
+                    nag_loc = post_id;
+
+                }
+
+            }
+
+            // for pages
+            if ( !value.search('page-id' ) ) {
+                
+                var classArray = value.split('-');
+                var post_id = parseInt( classArray[2] );
+
+                if ( post_id > 0 ) {
+
+                    nag_loc = post_id;
+
+                }
+
+            }
+
+        });
+
+        var data = {
+            action: 'leaky_paywall_store_nag_location',
+            post_id: nag_loc
+        };
+
+        $.get(leaky_paywall_script_ajax.ajaxurl, data, function(resp) {
+            window.location.href = url;
+        });
+
+    });
     
 });
